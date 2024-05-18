4b8c9198d987b156094ba4738715d444ed2a3448
perykasz <perykasz@d17-428-pc-06.ipa.ii.agh.edu.pl>
2024-05-18 11:14:30 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'yeahYeah';
+    const[email, setEmail] = useState('yeahYeah');
+
+    let message = <div>Too short</div>;
+    if(email.length < 5) {
+        message = <div>Is too short</div>;
+    } else if ( email.length < 15) {
+        message =<div>Is ok</div>;
+    } else {
+        message = <div>Too long</div>
+    }
 
     function  handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value)
     }
+
   return (
       <div>
         <h1>System do zapisów na zajęcia</h1>
           <h2>Your email {email}</h2>
-          <input type="text" onChange={handleChange}/>
+          {message}
+          <input type="text" value={email} onChange={handleChange}/>
       </div>
   );
 }
